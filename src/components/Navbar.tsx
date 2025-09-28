23dd2150e071f3ad90557471f7c2104a2d054374
qinflan <158785365+qinflan@users.noreply.github.com>
2025-09-28 19:11:29 -0400

    move theme toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,9 @@ const Navbar = () => {
   return (
     <div className="w-full ml-2 mt-6 fixed top-0 p-2 flex text-sm h-[100px] items-center gap-6">
         <h1 className="font-['articulat-cf'] font-bold tracking-tight text-foreground">shadersmith</h1>
-        <ModeToggle/>
+        <div className="right-0 fixed mr-12">
+          <ModeToggle/>
+        </div>
     </div>
   )
 }
