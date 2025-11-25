6b0890f36a09c87c963a600a254712e652ea63ee
Quinn Flanigan <quinn.flanigan001@umb.edu>
2025-11-25 14:30:54 -0500

    more responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ import GradientCard from './gradient/GradientCard'
 function App() {
 
   return (
-    <div className="min-h-screen flex flex-col items-center bg-background">
+    <div className="flex flex-col items-center bg-background">
       <Navbar />
         <div className="flex flex-col-reverse w-[90%] md:flex-row mt-[100px] md:mt-[150px] justify-center gap-10">
           <ControlPanel />
           <GradientCard />
       </div>
-          <div className="mt-[50px] mb-[50px] w-full">
+          <div className="p-[50px] w-screen bg-background">
             <p className="font-['helvetica-neue-lt-pro'] text-2xl font-normal tracking-tight">this is a demo, npm package + code generation for art coming soon...</p>
           </div>
     </div>
