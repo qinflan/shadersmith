8b1609521b6c94eee60036897041333f2875fb01
Quinn Flanigan <quinn.flanigan001@umb.edu>
2025-11-25 14:00:24 -0500

    theme style on background for page container

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ import GradientCard from './gradient/GradientCard'
 function App() {
 
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-background">
       <Navbar />
         <div className="flex mt-[150px] justify-center gap-10">
           <ControlPanel />
           <GradientCard />
       </div>
-          <div className="mt-[50px] mb-[50px]">
+          <div className="mt-[50px] mb-[50px] w-full">
             <p className="font-['helvetica-neue-lt-pro'] text-2xl font-normal tracking-tight">this is a demo, npm package + code generation for art coming soon...</p>
           </div>
     </div>
